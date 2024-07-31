e5403d839da84aed9a6656e8b1be5d525eed9059
Artem <art.sled.ua@gmail.com>
2024-08-01 01:03:47 +0300

    add

---
+++ raady.js
@@ -0,0 +1,12 @@
+// Create an arrow function to add list items to the array
+const addListItem = (item) => {
+    listItems.push(item);
+};
+
+// Call the arrow function to add list items
+addListItem("Item 4");
+addListItem("Item 5");
+addListItem("Item 6");
+
+// Print the updated list items
+console.log(listItems);
